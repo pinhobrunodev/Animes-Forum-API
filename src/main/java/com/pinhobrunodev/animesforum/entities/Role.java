176e945253d172ec092fd65bdec0e97f205539a1
pinhobrunodev <iaishowcs@gmail.com>
2021-10-25 11:28:04 -0300

    Role

---
+++ src/main/java/com/pinhobrunodev/animesforum/entities/Role.java
@@ -0,0 +1,62 @@
+package com.pinhobrunodev.animesforum.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    public Role() {
+
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void PrePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void PreUpdate() {
+        updatedAt = Instant.now();
+    }
+}
