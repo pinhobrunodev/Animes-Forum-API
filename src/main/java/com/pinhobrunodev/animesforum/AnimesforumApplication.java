df19697c21542fa050e1c043542e5efd793376ad
pinhobrunodev <iaishowcs@gmail.com>
2021-10-25 09:34:23 -0300

    First commit :sparkles:

---
+++ src/main/java/com/pinhobrunodev/animesforum/AnimesforumApplication.java
@@ -0,0 +1,13 @@
+package com.pinhobrunodev.animesforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimesforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimesforumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinhobrunodev/animesforum/AnimesforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinhobrunodev.animesforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimesforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
