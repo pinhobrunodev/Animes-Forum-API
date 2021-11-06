d77b4e400fa0bbaaf6edc446be0df85eec4e2876
pinhobrunodev <iaishowcs@gmail.com>
2021-11-06 17:43:44 -0300

    Unit Tests : Repository layer -> AnimeRepositoryTests ✅

--- src/test/java/com/pinhobrunodev/animesforum/tests/Factory.java
+++ src/test/java/com/pinhobrunodev/animesforum/tests/Factory.java
@@ -1,13 +1,21 @@
 package com.pinhobrunodev.animesforum.tests;
 
+import com.pinhobrunodev.animesforum.entities.Anime;
 import com.pinhobrunodev.animesforum.entities.Gender;
 
 import java.time.Instant;
+import java.time.LocalDate;
 
 public class Factory {
 
 
     public static Gender createGender(){
         return new Gender(1L,"Gender", Instant.now(),Instant.now());
+    }
+
+    public static Anime createAnime(){
+        Anime anime = new Anime(1L,"Title","description","imgUrl", LocalDate.now(),Instant.now(),Instant.now());
+        anime.getGenders().add(createGender());
+        return  anime;
     }
 }
